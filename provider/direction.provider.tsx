617ec290b4df3e0df593f646dcae0c08696898f7
ahmedss1997 <fatma.r@6bees.co>
2024-07-07 07:34:44 +0300

    redirect to dashboard

--- provider/direction.provider.tsx
+++ provider/direction.provider.tsx
@@ -2,11 +2,17 @@
 import React from "react";
 import { useThemeStore } from "@/store";
 import { DirectionProvider as RadixDirectionProvider } from "@radix-ui/react-direction";
+import { redirect } from "next/navigation";
+import {usePathname} from "next/navigation";
 
 const DirectionProvider = ({ children, lang }: { children: React.ReactNode; lang: string }) => {
   const { isRtl } = useThemeStore();
 
-  const direction = lang === "ar" || isRtl ? "rtl" : "ltr";
+  const direction = lang === "ar" || isRtl ? "rtl" : "ltr";  
+  const pathName = usePathname();
+  if (pathName === "/" + lang) {
+    redirect("/dashboard");
+  }
 
   return (
     <div dir={direction}>
